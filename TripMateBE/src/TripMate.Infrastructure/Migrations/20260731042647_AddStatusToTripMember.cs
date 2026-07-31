3233046145cf61aed9fd4e322aea1d50baa7ed75
HuuThang\thang <thanghh004@gmail.com>
2026-07-31 11:27:17 +0700

    migration

---
+++ TripMateBE/src/TripMate.Infrastructure/Migrations/20260731042647_AddStatusToTripMember.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripMate.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToTripMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "trip_members",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "trip_members");
+        }
+    }
+}
